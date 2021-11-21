1a3a5e82f72ff3b2292629a9f58b425d55825acb
LeoPederz <77103826+LeoPederz@users.noreply.github.com>
2021-11-21 17:45:14 +0100

    styled HeroElements

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import Navbar from '../Navbar'
-import {HeroContainer} from './HeroElements'
+import React from 'react';
+import Navbar from '../Navbar';
+import {HeroContainer,
+     HeroContent, 
+     HeroItems, 
+     HeroH1, 
+     HeroP, 
+     HeroBtn 
+    } from './HeroElements';
 
 const Hero = () => {
     return (
        <HeroContainer>
            <Navbar/>
+           <HeroContent>
+               <HeroItems>
+                   <HeroH1>Greatest pizza in the world</HeroH1>
+                   <HeroP> Ready in 60 seconds</HeroP>
+                   <HeroBtn>Place your order here</HeroBtn>
+               </HeroItems>
+           </HeroContent>
         </HeroContainer>
 
     );
